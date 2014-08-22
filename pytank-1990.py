74df869145f7018f09f93a85eeb8aa623cf08e26
Patryk Konopka <konopka90@gmail.com>
2014-08-22 19:00:00 +0200

    First serious commit

--- pytank-1990.py
+++ pytank-1990.py
@@ -1,19 +1,31 @@
 import sys
 import sdl2
 import sdl2.ext
+from pytank.game import *
 
+# Main loop
 
 def run():
     sdl2.ext.init()
-    window = sdl2.ext.Window("The Pong Game", size=(800, 600))
+    window = sdl2.ext.Window("PyTank 1990", size=(800, 600)) #, position=None, flags=sdl2.SDL_WINDOW_FULLSCREEN)
     window.show()
     running = True
+    
+    game = Game()
+    
     while running:
         events = sdl2.ext.get_events()
         for event in events:
             if event.type == sdl2.SDL_QUIT:
                 running = False
                 break
+				
+            if event.type == sdl2.SDL_KEYDOWN:
+                if event.key.keysym.sym == sdl2.SDLK_ESCAPE:
+                    running = False
+                    break
+                    
+        game.run()
         window.refresh()
     return 0
 
---
+++ pytank/game.py
@@ -0,0 +1,3 @@
+class Game:
+    def run(self):
+        print "Jazda swinie"
